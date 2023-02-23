e7efc0c42b5ac2ee15b28ffb89cd03e80609adbb
Fabio de Stefani <fabiostefani@gmail.com>
2023-02-23 06:29:44 -0300

    setup aplicação e conceitos de entidades

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # example_ddd_ts
 Projeto de exemplo de aplicação de DDD com TypeScript
+
+
+### Instalações
+npm i typescript --save-dev
+npm i tslint --save-dev
+
+### Criar o projeto
+npx tsc --init
+npx tslint --init
+
+Ajustar no arquivo tsconfig.json as propriedades
+    "incremental": true,                              
+    "outDir": "./dist",
+    Adicionar o seguinte bloco no final da estrutura do compiler options
+        "include": [
+            "src/**/*.ts"
+        ]
+
+### Para compilar
+npx tsc        
---
+++ src/entity/customer.ts
@@ -0,0 +1,46 @@
+class Customer {
+
+    _id: string;
+    _name: string;
+    _address: string = "";
+    _active: boolean = false;
+
+    constructor(id: string, name: string) {
+        this._id = id;
+        this._name = name;   
+        this.validate();     
+    }
+
+    validate() { 
+        if (this._name.length === 0) {
+            throw new Error("Name is required");
+        }
+
+        if (this._id.length === 0) {
+            throw new Error("Id is required");
+        } 
+    }
+
+
+    changeName(name: string) {
+
+        this._name = name;
+        this.validate();
+    }
+
+    activate() {
+        if (this._active) return;        
+        if (this._address.length === 0) {
+            throw new Error("Address is mandatory to activate a customer");
+        }
+        this._active = true;
+    }
+
+    deactivate() {  
+        if (!this._active) return;    
+        this._active = false;
+    }
+
+}
+
+let customer = new Customer("1", "");
